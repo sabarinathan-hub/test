65e600657178a41a541949f5ad4a100b52bc9452
msabari.bsc@gmail.com <msabari.bsc@gmail.com>
2026-07-11 19:38:31 +0530

    Revert "added the else part of username and password verification"
    This reverts commit aa7e8b1d14b3d73482d666c40ac296ac43f91e4d.

--- test.py
+++ test.py
@@ -3,5 +3,3 @@ username = "sabari"
 password = "sabari"
 if username == "sabari" and password == "sabari":
     print(f"Welcome {username} to the {msg}")
-else:
-    print("Please enter valid username and password")
