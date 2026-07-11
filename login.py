479d51749e2c93673cb201693d14f96445c41b2d
msabari.bsc@gmail.com <msabari.bsc@gmail.com>
2026-07-11 19:53:42 +0530

    new login file

---
+++ login.py
@@ -0,0 +1,5 @@
+usr_nick_name = input("Enter your nick name : ")
+usr_name = input("Enter the username : ")
+user_passwd = input("Ener the password : ")
+if usr_name == usr_name and user_passwd == user_passwd:
+    print(f"!!  Welcome {usr_nick_name} !!")
