4ec337e12a7d8281d44acb1fb42b2fac32982f9c
msabari.bsc@gmail.com <msabari.bsc@gmail.com>
2026-07-12 21:18:52 +0530

    Else part is added

--- login.py
+++ login.py
@@ -3,3 +3,5 @@ usr_name = input("Enter the username : ")
 user_passwd = input("Ener the password : ")
 if usr_name == usr_name and user_passwd == user_passwd:
     print(f"!!  Welcome {usr_nick_name} !!")
+else:
+    print("Invalid username or password")
